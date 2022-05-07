27d67c903084680e4fe517d7fc88aac6fbd34b68
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-05-06 22:41:09 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TimerService } from './timer-service.service';
+import { BitcoinService } from './bitcoin.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ],
-  providers:    [TimerService               ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  providers: [BitcoinService],
 })
-export class AppModule { }
+export class AppModule {}
