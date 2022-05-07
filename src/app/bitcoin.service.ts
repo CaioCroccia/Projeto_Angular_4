3cbba2d69f552bef25205fe67ff2a18397d81557
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-05-06 22:40:38 -0300

    1 file modified, 1 added, 1 deleted

---
+++ src/app/bitcoin.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface Response {
+  time: {
+    updated: string;
+  };
+  bpi: {
+    USD: {
+      symbol: string;
+      rate_float: number;
+    };
+    BRL: {
+      symbol: string;
+      rate_float: number;
+    };
+  };
+}
+
+@Injectable()
+export class BitcoinService {
+  current: Response;
+  list: Array<Response> = [];
+  private temporizador: any;
+
+  constructor(private http: HttpClient) {}
+
+  update() {
+    this.http
+      .get<Response>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
+      .subscribe((data) => {
+        this.current = data;
+        this.list.push(data);
+      });
+    if (!this.temporizador) {
+      this.temporizador = setInterval(() => {
+        this.http
+          .get<Response>(
+            'https://api.coindesk.com/v1/bpi/currentprice/BRL.json'
+          )
+          .subscribe((data) => {
+            if (data.bpi.USD.rate_float == this.current.bpi.USD.rate_float) {
+            } else {
+              this.current = data;
+              this.list.push(data);
+            }
+          });
+      }, 60000);
+    }
+  }
+}
